97654e13e174919b8467f6434ed1d1076b275887
hary85 <65687955+hary85@users.noreply.github.com>
2021-03-02 06:49:10 +0700

    complete todo function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Proptypes from "prop-types";
 
-const Todo = (Props) => {
+const Todo = ({ text, completeTodo, index, isCompleted }) => {
   return (
-    <div className="todo">
-      <span className="todo-text">{Props.text}</span>
+    <div className="todo" onClick={() => completeTodo(index)}>
+      <span
+        className="todo-text"
+        style={{ textDecoration: isCompleted ? "line-through" : "initial" }}
+      >
+        {text}
+      </span>
     </div>
   );
 };
 
 Todo.Proptypes = {
-  text: Proptypes.string.isRequired
+  text: Proptypes.string.isRequired,
+  completeTodo: Proptypes.func.isRequired,
+  index: Proptypes.number.isRequired,
+  isCompleted: Proptypes.bool.isRequired
 };
 export default Todo;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,11 +3,19 @@ import PropTypes from "prop-types";
 
 import Todo from "./Todo";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, completeTodo }) => {
   return (
     <section className="todos">
       {todos.map((todo, index) => {
-        return <Todo key={index} text={todo.text} />;
+        return (
+          <Todo
+            key={index}
+            text={todo.text}
+            isCompleted={todo.isCompleted}
+            completeTodo={completeTodo}
+            index={index}
+          />
+        );
       })}
     </section>
   );
@@ -18,6 +26,7 @@ Todos.propTypes = {
     PropTypes.shape({
       text: PropTypes.string
     })
-  )
+  ),
+  completeTodo: PropTypes.func.isRequired
 };
 export default Todos;
